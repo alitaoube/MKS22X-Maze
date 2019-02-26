e272c5dc8ac093a133c263fe4b5d90c08d76aeb5
Ali Taoube <ataoube00@stuy.edu>
2019-02-26 10:00:16 -0500

    Successfully tested the read method, prints all characters

--- Maze.java
+++ Maze.java
@@ -1,13 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Maze{
+	public static void main(String[] args){
+		read(args[0]);	
+	}
 
-
-  public static read(String filename){
+  public static void read(String filename){
+	try{
 		File text = new File(filename);	
 
 		Scanner inf = new Scanner(text);
 
-		while (inf.hastNext()){
+		while (inf.hasNext()){
 			System.out.println(inf.next());
 		}
+	}
+	catch(FileNotFoundException e){
+		System.out.println("Gotta catch 'em all");
+	}
 	}
 }
